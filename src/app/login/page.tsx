2e8cd0a391f8a1506e002109b23b3702c2ade751
JUNSEOK <kd02109@gmail.com>
2024-05-29 12:22:10 +0900

    feat: 로그인 시 쿠키 설정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { createCookie } from '@/app/lib/cookies';
 import type { FormProps } from 'antd';
 import { Button, Checkbox, Form, Input } from 'antd';
+import { useRouter } from 'next/navigation';
 
 import styles from './login.module.css';
 
@@ -10,32 +12,57 @@ type FieldType = {
   password?: string;
 };
 
-const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
-  const res = await fetch('http://52.79.165.249:8000/user/login', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify(values),
-  });
-  const data: {
-    accessToken: string;
-    refreshToken: string;
-  } = await res.json();
-  // 쿠키에 token 값 모두 저장 Next애서 만들어서 관리
-  // middleware에서 쿠키로 파악하여 /login, / 라우트 관리
-  // fetch 요청 분리
-  console.log(data);
-  console.log('Success:', values);
-};
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = async (
-  errorInfo,
-) => {
-  console.log('Failed:', errorInfo);
-};
-
 export default function Page() {
+  const router = useRouter();
+  const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+    try {
+      const res = await fetch('https://dpi.swifty.kr/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+      console.log(res.status, res);
+      const data: {
+        accessToken: string;
+        refreshToken: string;
+      } = await res.json();
+
+      // 쿠키 생성
+      await createCookie({
+        name: 'swifty-access',
+        value: data.accessToken,
+        option: {
+          maxAge: 100 * 60 * 60 * 24,
+        },
+      });
+      await createCookie({
+        name: 'swifty-refresh',
+        value: data.refreshToken,
+        option: {
+          maxAge: 100 * 60 * 60 * 24 * 10,
+        },
+      });
+
+      router.replace('/');
+    } catch (err) {
+      if (err instanceof Error) {
+        throw new Error(err.message);
+      }
+    }
+
+    // 쿠키에 token 값 모두 저장 Next애서 만들어서 관리
+    // middleware에서 쿠키로 파악하여 /login, / 라우트 관리
+    // fetch 요청 분리
+  };
+
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = async (
+    errorInfo,
+  ) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
     <main className={styles.main}>
       <Form
