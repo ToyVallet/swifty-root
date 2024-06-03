c82b9d9a47375a4411bb800de1c9344a0f9f2a01
JUNSEOK <kd02109@gmail.com>
2024-06-03 15:23:39 +0900

    feat: 서버 api const obj 생성

--- src/app/lib/contants.ts
+++ src/app/lib/contants.ts
@@ -2,3 +2,28 @@ export const COOKIE_KEYS = {
   accessToken: 'swifty-access',
   refreshToken: 'swifty-refresh',
 } as const;
+
+export const SERVER_URL = 'https://dpi.swifty.kr';
+
+export const API = {
+  admin: {
+    lineup: {
+      lineups: '/admin/line_up',
+      lineupSubId: (id: string) => `/admin/line_up/${id}`,
+    },
+    client: {
+      host: `/admin/host`,
+      hostSubid: (id: string) => `/admin/host/${id}`,
+    },
+  },
+  festival: {
+    festivals: '/admin/festival',
+    festivalSubid: (id: string) => `/admin/festival/${id}`,
+    detail: (id: string) => `/admin/festival/detail/${id}`,
+  },
+  search: {
+    keyword: (keyword: string) => `/festival/search?keyword=${keyword}`,
+    q: `festival/search`,
+  },
+  login: '/user/login',
+} as const;
